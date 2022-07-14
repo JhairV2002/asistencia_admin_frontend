40d2424a58405a24f8f84cb8564bf23666e1c363
Roberto Obregon <81327803+Fenixx87@users.noreply.github.com>
2022-07-14 11:09:28 -0500

    frontend

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DiaFestivoComponent } from './diaFestivo/diaFestivo.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'diaFestivo', component:DiaFestivoComponent},
+  {path: 'diaFestivo/:id', component:DiaFestivoComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
