0916e6df71d2a652157f3fc182944b3489f759d7
1752035343 <jag.vasquez@yavirac.edu.ec>
2022-07-14 11:23:43 -0500

    crud completo y funcional

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RolesComponent } from './roles/roles.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'roles',
+    component: RolesComponent,
+  },
+  {
+    path: 'roles/:id',
+    component: RolesComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ src/app/roles/roles.component.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Rol } from './roles';
+import { RolesService } from './roles.service';
+
+@Component({
+  selector: 'app-roles',
+  templateUrl: './roles.component.html',
+  styleUrls: ['./roles.component.css'],
+})
+export class RolesComponent implements OnInit {
+  constructor(
+    private rolesService: RolesService,
+    private activatedRoute: ActivatedRoute
+  ) {}
+
+  initialForm: Rol = {
+    id: 0,
+    name: '',
+    admin: false,
+    created: new Date(),
+    updated: new Date(),
+    enable: false,
+    archived: false,
+  };
+
+  form: Rol = this.initialForm;
+
+  ngOnInit(): void {
+    this.activatedRoute.paramMap.subscribe((params) => {
+      if (params.get('id')) {
+        this.findById(parseInt(params.get('id')!));
+      }
+    });
+  }
+
+  save(): void {
+    this.rolesService.save(this.form).subscribe(() => {
+      this.form = this.initialForm;
+    });
+  }
+
+  findById(id: number): void {
+    this.rolesService.findById(id).subscribe((res) => {
+      this.form = res;
+    });
+  }
+  deleteById(): void {
+    this.rolesService.deleteById(this.form.id).subscribe(() => {
+      console.log('Borrado');
+    });
+  }
+}
