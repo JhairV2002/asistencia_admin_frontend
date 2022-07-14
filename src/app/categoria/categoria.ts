de24ccdab3c41309c6e4b3c3fe21b7ab5ddd3c61
Jonathan Caiza <you@example.com>
2022-07-13 23:30:40 -0500

    Categoria-Actividad

---
+++ src/app/categoria/categoria.ts
@@ -0,0 +1,12 @@
+
+export interface Categoria {
+    categoriaActividadId: number,
+    nombre: string,
+    secuencia: string,
+    color: string,
+    created: Date,
+    updated: Date,
+    enabled: boolean,
+    archived: boolean
+}
+
