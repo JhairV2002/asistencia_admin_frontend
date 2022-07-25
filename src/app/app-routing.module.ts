1399b91cfd1c5022952221725ca51253649ced3b
1752035343 <jag.vasquez@yavirac.edu.ec>
2022-07-24 21:39:11 -0500

    merge with dias_festivos branch

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,22 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RolesComponent } from './roles/roles.component';
-import { DiaFestivoComponent } from './diaFestivo/diaFestivo.component';
+import { DiaFestivoFormComponent } from './feature/diaFestivo/form/diaFestivo.form.component';
 import { CategoriaComponent } from './categoria/categoria.component';
 import { ActividadComponent } from './actividad/actividad.component';
 
 const routes: Routes = [
   { path: 'roles', component: RolesComponent },
   { path: 'roles/:id', component: RolesComponent },
-  { path: 'diaFestivo', component: DiaFestivoComponent },
-  { path: 'diaFestivo/:id', component: DiaFestivoComponent },
+  { path: 'diaFestivo', component: DiaFestivoFormComponent },
+  { path: 'diaFestivo/:id', component: DiaFestivoFormComponent },
   { path: 'categoria', component: CategoriaComponent },
   { path: 'categoria/:id', component: CategoriaComponent },
-  { path: 'actividad', component:ActividadComponent},
-  { path: 'actividad/:id', component:ActividadComponent},
-  {path: '', redirectTo: '/layout', pathMatch: 'full'},
-  {path: 'layout', loadChildren:() => import('./layout/layout.module').then(m => m.LayoutModule)},
-  {path: 'feature', loadChildren:() => import('./feature/feature.module').then(m => m.FeatureModule)}
+  { path: 'actividad', component: ActividadComponent },
+  { path: 'actividad/:id', component: ActividadComponent },
+  { path: '', redirectTo: '/layout', pathMatch: 'full' },
+  {
+    path: 'layout',
+    loadChildren: () =>
+      import('./layout/layout.module').then((m) => m.LayoutModule),
+  },
+  {
+    path: 'feature',
+    loadChildren: () =>
+      import('./feature/feature.module').then((m) => m.FeatureModule),
+  },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,31 +3,9 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
-import { RolesComponent } from './roles/roles.component';
-import { DiaFestivoComponent } from './diaFestivo/diaFestivo.component';
-import { CategoriaComponent } from './categoria/categoria.component';
-import { ActividadComponent } from './actividad/actividad.component';
-=======
->>>>>>> origin/dias_feriados
 import { HttpClientModule } from '@angular/common/http';
 @NgModule({
-  declarations: [
-    AppComponent,
-<<<<<<< HEAD
-    DiaFestivoComponent,
-    CategoriaComponent,
-    RolesComponent,
-    ActividadComponent,
-=======
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FormsModule,
-    HttpClientModule
->>>>>>> origin/dias_feriados
-  ],
+  declarations: [AppComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
